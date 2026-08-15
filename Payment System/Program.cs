866a1b7b3fc50f079134080ad95c0b199f4775cd
20claranasser <Claranasser95@gmail.com>
2026-08-15 18:50:20 +0300

    Add project files.

---
+++ Payment System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Payment_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
